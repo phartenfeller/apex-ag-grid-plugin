0cba45662758b045ef99b3d425cca103053c1932
phartenfeller <pharti@hotmail.de>
2023-04-27 20:31:22 +0200

    setup for offline build

--- region_plugin/js/webpack.prod.js
+++ region_plugin/js/webpack.prod.js
@@ -2,32 +2,42 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { DefinePlugin } = require('webpack');
 
 const entry = path.resolve(__dirname, 'src', 'index.js');
 
-module.exports = {
-  entry: {
-    index: entry,
-    'index.min': entry,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
+module.exports = (env) => {
+  const offlineMode = !!env.offline;
+  // eslint-disable-next-line no-console
+  console.log('Offline mode enabled:', env.offline);
+
+  return {
+    entry: {
+      index: entry,
+      'index.min': entry,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({ filename: 'index.css' }),
+      new DefinePlugin({ OFFLINE_MODE: offlineMode }),
     ],
-  },
-  plugins: [new MiniCssExtractPlugin({ filename: 'index.css' })],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({ test: /\.min\.js$/ }),
-      new CssMinimizerPlugin({ test: /\.min\.css$/ }),
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-  },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({ test: /\.min\.js$/ }),
+        new CssMinimizerPlugin({ test: /\.min\.css$/ }),
+      ],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+    },
+  };
 };
